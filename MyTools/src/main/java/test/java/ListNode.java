64816c4701cdbc70361911ce9df453b8b8762923
guoff28031 <guoff28031@hundsun.com>
2021-01-05 15:31:47 +0800

    新增算法题

--- MyTools/src/main/java/test/java/ListNode.java
+++ MyTools/src/main/java/test/java/ListNode.java
@@ -2,17 +2,18 @@ package test.java;
 
 /**
  * 文件描述
+ * *
+ * * @ProductName: 现金管理5.0
+ * * @ProjectName: MyTools
+ * * @Package: test.java
  *
- * @ProductName: 现金管理5.0
- * @ProjectName: MyTools
- * @Package: test.java
- * @Description:
- * @Author: guoff28031
- * @CreateDate: 2020/10/13 10:27
- * @UpdateUser: guoff28031
- * @UpdateDate: 2020/10/13 10:27
- * @UpdateRemark:
- * @Version: 5.0.0
+ * * @Description:
+ * * @Author: guoff28031
+ * * @CreateDate: 2020/10/13 10:27
+ * * @UpdateUser: guoff28031
+ * * @UpdateDate: 2020/10/13 10:27
+ * * @UpdateRemark:
+ * * @Version: 5.0.0
  * <p>
  * Copyright © 2020 Hundsun Technologies Inc. All Rights Reserved
  **/
--- MyTools/src/main/java/test/java/Test.java
+++ MyTools/src/main/java/test/java/Test.java
@@ -1,7 +1,9 @@
 package test.java;
 
 
+import javax.sound.midi.Soundbank;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -21,22 +23,34 @@ public class Test {
 
     public static void main(String[] args) {
         String[] A = {"cool", "lock", "cook"};
-        ListNode listNode = new ListNode(1);
-        ListNode dummyNode = listNode;
-        for (int i = 2; i < 5; i++) {
-            dummyNode.next = new ListNode(i);
-            dummyNode = dummyNode.next;
-        }
-        String S = "a#c";
-        String T = "b";
-        System.out.println(backspaceCompare(S, T));
+        int[] a = {2,7,4,1,8,1};
+        String s = "aaa";
+        System.out.println(largeGroupPositions(s));
+        System.out.println((a));
     }
 
-    public static void displayBackward(ListNode node) {
-        if (node != null) {
-            displayBackward(node.next);
-            System.out.println(node.val);
+    public static List<List<Integer>> largeGroupPositions(String s) {
+        List<List<Integer>> result = new ArrayList();
+        int start = 0, end = 0;
+        for(int i = 1; i < s.length(); i++) {
+            if(s.charAt(i-1) == s.charAt(i)){
+                if(start == 0){
+                    start = i-1;
+                } else{
+                    end = i;
+                }
+                if ((end-start)>=2 && i==(s.length()-1)) {
+                    result.add(Arrays.asList(start,end));
+                }
+            } else{
+                if ((end-start)>=2){
+                    result.add(Arrays.asList(start,end));
+                }
+                start=0;
+                end=0;
+            }
         }
+        return result;
     }
 
     /**
@@ -154,4 +168,134 @@ public class Test {
         }
         return true;
     }
+
+    public static int islandPerimeter(int[][] grid) {
+        int length = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == 1) {
+                    if (i > 0 && j > 0 && i < grid.length - 1 && j < grid[i].length) {
+                        if (grid[i - 1][j] == 0 || grid[i + 1][j] == 0 || grid[i][j - 1] == 0 || grid[i][j + 1] == 0) {
+                            length++;
+                        }
+                        length++;
+                    }
+                }
+            }
+        }
+        return length;
+    }
+
+    /**
+     * 给出一个无重叠的 ，按照区间起始端点排序的区间列表。
+     * <p>
+     * 在列表中插入一个新的区间，你需要确保列表中的区间仍然有序且不重叠（如果有必要的话，可以合并区间）。
+     *
+     * @param intervals
+     * @param newInterval
+     * @return
+     */
+    public static int[][] insert(int[][] intervals, int[] newInterval) {
+        List<Integer> result = new ArrayList();
+        int left = newInterval[0];
+        int right = newInterval[1];
+        boolean end = false;
+        for (int[] interval : intervals) {
+            if (interval[1] < left) {
+                result.add(interval[0]);
+                result.add(interval[1]);
+            } else if (interval[0] > right) {
+                if (!end) {
+                    result.add(left);
+                    result.add(right);
+                    end = true;
+                }
+                result.add(interval[0]);
+                result.add(interval[1]);
+            } else {
+                left = Math.min(left, interval[0]);
+                right = Math.max(right, interval[1]);
+            }
+        }
+        if (!end) {
+            result.add(left);
+            result.add(right);
+        }
+        int[][] ans = new int[result.size() / 2][2];
+        for (int i = 0; i < result.size(); i = i + 2) {
+            ans[i / 2][0] = result.get(i);
+            ans[i / 2][1] = result.get(i + 1);
+        }
+        return ans;
+    }
+
+    public static ListNode insertionSortList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode cur = head.next;
+        ListNode dummyNode = new ListNode(0);
+        dummyNode.next = head;
+        ListNode lastSorted = head;
+        while (cur != null) {
+            if (lastSorted.val <= cur.val) {
+                lastSorted = lastSorted.next;
+            } else {
+                ListNode prev = dummyNode;
+                while (cur.val >= prev.next.val) {
+                    prev = prev.next;
+                }
+                lastSorted.next = cur.next;
+                cur.next = prev.next;
+                prev.next = cur;
+            }
+            cur = lastSorted.next;
+        }
+        return dummyNode.next;
+    }
+
+    /**
+     * 上升下降字符串
+     */
+    public static String sortString(String s) {
+        int[] buckets = new int[26];
+        for (int i = 0; i < 26; i++) {
+            buckets[s.charAt(i) - 'a']++;
+        }
+        StringBuffer sb = new StringBuffer();
+        while (sb.length() < s.length()) {
+            for (int i = 0; i < 26; i++) {
+                if (buckets[i] > 0) {
+                    sb.append(buckets[i]);
+                    buckets[i]--;
+                }
+            }
+            for (int j = 25; j >= 0; j--) {
+                if (buckets[j] > 0) {
+                    sb.append(buckets[j]);
+                    buckets[j]--;
+                }
+            }
+        }
+        return sb.toString();
+    }
+
+    public static char findTheDifference(String s, String t) {
+        char[] s1 = s.toCharArray();
+        char[] t1 = t.toCharArray();
+        int[] bucket = new int[26];
+        for (int i = 0; i < s1.length; i++) {
+            bucket[s1[i] - 'a']++;
+        }
+        for (int i = 0; i < t1.length; i++) {
+            bucket[t1[i] - 'a']--;
+        }
+        int result = 0;
+        for (int i = 0; i < bucket.length; i++) {
+            if (bucket[i] > 0) result = i;
+        }
+        return (char) ('a' + result);
+    }
+
+
 }
